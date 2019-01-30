0685cb802c6eb8aee4e4cf43b6481fa756da107b
Rodney Solomon <rodneysolomonjr@gmail.com>
2019-01-30 10:01:28 -0500

    Login and Logout Finished

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import jwt_decode from "jwt-decode";
 import setAuthToken from "./utils/setAuthToken";
-import { setCurrentUser } from "./actions/authActions";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
 import { Provider } from "react-redux";
 import store from "./store";
 
@@ -15,14 +15,24 @@ import Login from "./components/auth/Login";
 
 import "./App.css";
 
-// Check for token
+// Check for token - if there's a token, set it as the authenticated
 if (localStorage.jwtToken) {
-  // Set auth token header auth
+  // Set auth token header authority
   setAuthToken(localStorage.jwtToken);
   // Decode the token and get user info and expiration
   const decoded = jwt_decode(localStorage.jwtToken);
   // Set user and isAuthenticated
   store.dispatch(setCurrentUser(decoded));
+
+  // Check for expired token
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
+    // Logout the user
+    store.dispatch(logoutUser());
+    // TODO: Clear current Profile
+    // Redirect to login
+    window.location.href = "/login";
+  }
 }
 
 class App extends Component {
--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -51,3 +51,15 @@ export const setCurrentUser = decoded => {
     payload: decoded
   };
 };
+
+// Log user out
+export const logoutUser = () => dispatch => {
+  // remove token from local storage
+  localStorage.removeItem("jwtToken");
+
+  // remove the auth header for future requests
+  setAuthToken(false);
+
+  // set the current user to an empty object which will set isAuthenticated to false
+  dispatch(setCurrentUser({}));
+};
